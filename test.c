bdeb9ca86dd33abcde5379948790f19132a1c7b6
aqShanto <abdulquymshanto@gmail.com>
2024-11-13 01:50:09 +0600

    up

--- test.c
+++ test.c
@@ -1,6 +1,19 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 int main() {
-    printf("#");
+    char ab[] = "||||||||||||||||||||||||||||||||||||";
+    char ar[] = "||||   Welcome to our project   ||||";
+    printf("\n\n\t\t%s\n", ab);
+    printf("\t\t");
+    for (int i = 0; i <= sizeof(ar); i++) {
+        Sleep(10);  // 1 second delay
+        printf("%c", ar[i]);
+    }
+    printf("\n");
+    printf("\t\t%s\n", ab);
+    printf("\n\n");
     return 0;
 }
