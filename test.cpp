ef1bfad07b6ee4259d4068365595182a4afd5191
aqShanto <abdulquymshanto@gmail.com>
2024-12-02 13:34:03 +0600

    up

--- test.cpp
+++ test.cpp
@@ -1,38 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-}* root;
-
-typedef struct Node newnode;
-
 int main() {
-    root = (newnode*)malloc(sizeof(newnode));
-    int N;
-    scanf("%d", &N);
-    newnode* Head;
-    Head = (newnode*)malloc(sizeof(newnode));
-    Head = root;
-    int num;
-    scanf("%d", &num);
-    root->data = num;
-    for (int i = 1; i < N; i++) {
-        scanf("%d", &num);
-        while (Head->left != NULL && Head->right != NULL) {
-            if (Head->data <= num) {
-                Head = Head->left;
-            } else {
-                Head = Head->right;
-            }
-        }
-        if (Head->data <= num) {
-            Head->left->data = num;
-        } else {
-            Head->right->data = num;
-        }
-    }
-    return 0;
+    int n;
+    scanf("%d", &n);
+    char
 }
